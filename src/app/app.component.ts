a2de4460e1a3d05a663965e73501333866f71793
QuinntyneBrown <quinntynebrown@gmail.com>
2021-11-20 10:46:36 -0500

    example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component, Injectable } from '@angular/core';
-import { Store } from '@core/store/store';
+import { Store } from '../app/@core/store/store';
+
 import { of } from 'rxjs';
+import { delay, switchMap } from 'rxjs/operators';
 
 
 @Injectable()
 class AppStore extends Store {
   public get() {
-    return super.from$(() => of("Query Store"));
+    return super.from$(() => of(Date()),"DATE");
+  }
+
+  public refresh() {
+    return super.withRefresh(of(true),["DATE"]);
   }
 }
 
@@ -19,13 +25,21 @@ class AppStore extends Store {
   ]
 })
 export class AppComponent {
-  constructor(
-    private readonly _appStore: AppStore
-  ) {
+  constructor(private readonly _appStore: AppStore) {}
 
+  public title1$: any = of(true).pipe(delay(1000),switchMap(_ => this._appStore.get()));
+
+  public title2$: any = of(true).pipe(delay(2000),switchMap(_ => this._appStore.get()));
+
+  public title3$: any = of(true).pipe(delay(3000),switchMap(_ => this._appStore.get()));
+
+  // public title1$: any = of(true).pipe(delay(1000),switchMap(_ => of(Date())));
+
+  // public title2$: any = of(true).pipe(delay(2000),switchMap(_ => of(Date())));
+
+  // public title3$: any = of(true).pipe(delay(3000),switchMap(_ => of(Date())));
+
+  public refresh() {
+    this._appStore.refresh().subscribe();
   }
-
-  public appName$ = this._appStore.get();
-
-
 }
